306f3eed7c47a9aa00de9ac15604e7be75b769a2
Jaxon <jaxon_janes@yahoo.com>
2025-04-14 20:13:32 -0400

    Final updates for today

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,12 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss',
+  styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
-  openEmail(): void {
-    window.location.href = 'mailto:jaxon_janes@yahoo.com?subject=Job Opportunity&body=Hello Jaxon,';
-  }
-}
+export class HomeComponent {}
